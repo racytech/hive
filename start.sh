5c4b345adbcdf50ac25630099c51b4fe70106790
racytech <racytech0x0@gmail.com>
2025-03-20 19:30:26 +0500

    save

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # kurtosis run . --args-file dev.yaml --image-download always
-kurtosis run . --args-file dev.yaml
+kurtosis run . --args-file eof-devnet-0.yaml --image-download always
