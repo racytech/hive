7df8fa563f191fb4acb3ca88e7ca4d78d96dc10f
racytech <racytech0x0@gmail.com>
2025-04-03 06:05:32 +0500

    local erigon dev setup

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # kurtosis run . --args-file dev.yaml --image-download always
-kurtosis run . --args-file eof-devnet-0.yaml --image-download always
+# kurtosis run . --args-file eof-devnet-0.yaml --image-download always
+kurtosis run . --args-file eof-devnet-paritosh.yaml --image-download always
